c5d35eac5b87287669c87c724444d3a499e5a6d6
sr-pato <felipe.adeildo@proton.me>
2023-12-23 10:03:07 -0300

    Add `CloneChat` class Controller

--- clonechat.py
+++ clonechat.py
@@ -1,8 +1,58 @@
 import asyncio
 
+from telethon import TelegramClient
+from telethon.tl.patched import Message
+
 from utils import get_args
 from utils import get_chat
 from utils import get_client
+
+
+class CloneChat:
+    """Controller for CloneChat
+    
+    Methods:
+        clone(self): Clone Target Chat to Output Chat
+    """
+
+    def __init__(self, client: TelegramClient, target_id: int, output_id: int):
+        self.client = client
+        self.target_id = target_id
+        self.output_id = output_id
+
+    async def __get_chats(self):
+        """Initialize Target and Output Chat"""
+        self.target = await get_chat(self.client, self.target_id)
+        self.output = await get_chat(self.client, self.output_id)
+        del self.target_id, self.output_id
+
+    async def clone(self):
+        """Start the clonation process"""
+        await self.__get_chats()
+        await self.__clone_messages()
+
+    async def __clone_message(self, message: Message):
+        """Clonator wrapper method. This method decides wich method will be
+            used for clonation
+        
+        Args:
+            message (Message): The message to be cloned
+        """
+
+        clone_methods = {
+            'forward': ...,
+            'download': ...,
+        }
+
+        # TODO: Add conditionals to choice the clone method.
+
+    async def __clone_messages(self):
+        """Iterate over messages and call the wrapper clonator method."""
+        messages = self.client.iter_messages(self.target)
+
+        async for message in messages:
+            await self.__clone_message(message)
+            break
 
 
 async def main():
@@ -10,10 +60,7 @@ async def main():
     target_id, output_id = args.i, args.o
     client = await get_client()
 
-    target = await get_chat(client)(target_id)
-    output = await get_chat(client)(output_id)
-    print(f"Target: {target.title}")
-    print(f"Output: {output.title}")
+    await CloneChat(client, target_id, output_id).clone()
 
 
 if __name__ == "__main__":
